ca0012d21fa363900c59c2a8a0735ac5009ae085
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-09-11 18:53:35 +0200

    refactor

--- Question5/age_calculator.py
+++ Question5/age_calculator.py
@@ -1,16 +1,11 @@
 def calculateAge(bd, bm, by, d, m, y):
     years = y - by
     months = m - bm
-    if months < 0:
-        months += 12
-        years -= 1
     days = d - bd
-    if days < 0:
-        days += 30
-        months -= 1
     age = years + (months/12) + (days/365)
     return round(age)
 
 
 if __name__ == '__main__':
     print(calculateAge(15, 8, 1996, 11, 9, 2022))
+    print(calculateAge(15, 4, 1998, 25, 1, 2020))
